ee485897f57f01d8a3deb79fc6e1a9dca513788d
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-12 16:22:59 -0400

    view engine

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 // Dependencies
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 const dotenv = require('dotenv');
+const bodyparser = require('body-parser');
+const path = require('path');
+const { dirname } = require('path');
+const { json } = require('body-parser');
 
 // secret path
 dotenv.config({path:'config.env'});
 // port for others
 const port = process.env.port || 8080
 
+// log request
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+// set view engine 
+app.set("view engine", "ejs");
+
+
+
+
+
+
+
+
+//routes
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
 
