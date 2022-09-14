2e936b39c8258e26162127b0d639ceda1c91270b
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-13 23:50:17 -0400

    creating and saving new player

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,32 @@
 const Userdb = require('../model/model');
 
-// create and save new user
+// create and save new player
 exports.create = (req, res) => {
+    // create and save new user
+    if(!req,body){
+        res.status(400).send({message:"content can not be empty!"});
+        return;
+    }
+    // new player
+    const user = new Userdb({
+        Player: req.body.Player,
+        Age: req.body.Age,
+        Team: req.body.Team,
+        Position: req.body.Position
+        
+    })
 
+    // save player in the database
+    user
+     .save(user)
+     .then(data => {
+        res.send(data)
+     })
+     .catch(err =>{
+        res.status(500).send({
+            message : err.message || "some error occurred while creating a create operation"  
+        });
+     });
 }
 
 // retrieve and return all users / retrieve and return a single user
@@ -23,6 +47,6 @@ exports.update = (req, res) => {
 // delete a user by specific id in the request 
 
 exports.delete = (req, res) => {
-    
+
 }
 
