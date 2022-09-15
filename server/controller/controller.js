8615aa7b55a46f75d2d748f80eaeecbc9f8d0ce6
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-09-15 01:54:59 -0400

    Delete succesful

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -70,9 +70,28 @@ exports.update=(req,res)=>{
 
 // delete a user by specific id in the request 
 
-exports.delete = (req, res) => {
+exports.delete=(req, res)=>{
+  const id =req.params.id;
 
+  Userdb.findByIdAndDelete(id)
+  .then(data=>{
+    if(!data){
+      res.status(404).send({message: `Cannot Delete with id ${id}.Maybe id is wrong`})
+    }else{
+      res.send({
+        message:"User was deleted successfully!"
+      })
+    }
+  })
+  .catch(err=>{
+    res.status(500).send({
+      message:"Could not delete User with id="+id
+
+    });
+
+  });
 }
 
 
 
+  
